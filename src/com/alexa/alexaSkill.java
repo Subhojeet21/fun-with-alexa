2ba51eec6fed08a2e4c153698886cd1283a57111
Subhojeet Ghosh <43098066+Subhojeet21@users.noreply.github.com>
2020-03-21 16:58:44 +0530

    Update alexaSkill.java

--- src/com/alexa/alexaSkill.java
+++ src/com/alexa/alexaSkill.java
@@ -35,7 +35,12 @@ public class alexaSkill{
 		OutputSpeech resp1 = new OutputSpeech();
 		resp1.type = "SSML";
 		//resp1.ssml = "<speak><lang xml:lang=\"hi-IN\">आ गए बड़े लोग.</lang></speak>";
-		resp1.ssml = "<speak><emphasis level=\"strong\">oh my God look, who is here. <break time=\"1s\"/>Mr. Ghosh, the busy guy has arrived.</emphasis></speak>";
+		//resp1.ssml = "<speak><emphasis level=\"strong\">oh my God look, who is here. <break time=\"1s\"/>Mr. Ghosh, the busy guy has arrived.</emphasis></speak>";
+		resp1.ssml = "<speak>Hey buddy,<break time=\"1s\"/> this is a pandemic situation due to <emphasis level=\"strong\">Novel Corona virus. You should not travel anywhere this time.</emphasis> Our government and all health care professionals ";
+		resp1.ssml += "around the world are doing their best to control this situation. Now its our duty and responsibility to do out part. <emphasis level=\"strong\">Stay home. Stay safe. Work from home. ";
+		resp1.ssml += "Wash your hands often with soap and water or use hand sanitizer. Do not panic unnecessarily. Do not spread false news. Help elderly people.</emphasis>";
+		resp1.ssml += "We are all in this together and if we follow all the precautionary measures and maintain social distancing, we all will get out of this situation soon.</speak>";
+
 		
 		Response rp = new Response();
 		rp.outputSpeech = resp1;
